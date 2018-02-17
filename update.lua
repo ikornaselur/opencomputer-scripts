10f6c72c33d0e6d9a79c5652e248ebf8f228bd48
Axel Örn Sigurðsson <axel@takumi.com>
2018-02-17 16:31:55 +0100

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,12 +1,15 @@
 local internet = require("internet")
 
+
 local baseUrl = "https://raw.githubusercontent.com/ikornaselur/opencomputer-scripts/master/"
 
-for file in internet.request(baseUrl .. "files") do
+
+local response = internet.request(baseUrl .. "files")()
+
+-- Split file list on new lines
+for file in response:gmatch("[^\r\n]+") do
   print("Updating " .. file)
   local f, reason = io.open(file, "w")
-  for line in internet.request(baseUrl .. file) do
-    f:write(line)
-  end
+  f:write(internet.request(baseUrl .. file)())
   f:close()
 end
