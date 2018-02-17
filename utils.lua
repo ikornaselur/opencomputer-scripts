0782c4d0d6410b52a7b0fa513aa56ef65dfa6377
Axel Örn Sigurðsson <axel@takumi.com>
2018-02-17 16:44:51 +0100

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,8 +1,12 @@
 local keyboard = require("keyboard")
 
 
+local utils = {}
+
 function utils.check_break()
   if keyboard.isKeyDown(keyboard.keys.w) and keyboard.isControlDown() then
     os.exit()
   end
 end
+
+return utils
